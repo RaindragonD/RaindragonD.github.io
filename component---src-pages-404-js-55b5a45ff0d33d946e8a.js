153f6945e7ea7b50547aa17562360ab1102260ff
RaindragonD <yl4095@columbia.edu>
2025-02-07 08:15:41 -0700

    force update

---
+++ component---src-pages-404-js-55b5a45ff0d33d946e8a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkNowhereLog=self.webpackChunkNowhereLog||[]).push([[883],{429:function(e,t,a){a.r(t);var n=a(7294),l=a(8678),o=a(8183);t.default=e=>{let{data:t,location:a}=e;const s=t.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:s},n.createElement(o.Z,{title:"404: Not Found"}),n.createElement("h1",null,"Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-55b5a45ff0d33d946e8a.js.map
